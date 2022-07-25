b3cb81fa8d1fbb60b694a4fcd503fbb00fdf570f
Nataliya Novak <novak.natali@gmail.com>
2022-07-25 16:55:51 +0300

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from '../images/gallery-items';
+
 // Change code below this line
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+console.log(gallery);
+const galleryMarkup = createGallery(galleryItems);
+console.log(galleryMarkup);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup)
+
+function createGallery(items) {
+    return items.map(({original, preview, description}) => {
+       return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    }).join('')
+}
+gallery.addEventListener('click', onItemGalleryClick);
+function onItemGalleryClick(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains("gallery__image")) {
+        return
+    }
+    var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, enableKeyboard: true, docClose:true, loop:true});
+    
+     }
 
 console.log(galleryItems);
