2e6b4c07d226d05ec16915cd73edfd1e108f160a
Nataliya Novak <novak.natali@gmail.com>
2022-07-25 17:00:40 +0300

    3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import throttle from "lodash.throttle";
+import Player from "@vimeo/player"
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
 
+player.on('timeupdate', throttle(getTime, 100));
+function getTime(currentTime) {
+     localStorage.setItem("videoplayer-current-time", currentTime.seconds)   
+};
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from "lodash.throttle";
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('input'),
+    textarea: document.querySelector('textarea')
+};
+ refs.form.addEventListener('submit', onSubmitClick);
+refs.form.addEventListener('input', throttle(inFormInput, 500));
+refs.email.addEventListener('focus', onFormClick)
+refs.textarea.addEventListener('focus', onFormClick)
+const FEEDBACK = 'feedback-form-state';
+const formInput = {};
+refs.email = "";
+refs.textarea = "";
+
+function inFormInput(evt) {
+
+    formInput[evt.target.name] = evt.target.value;
+    localStorage.setItem("FEEDBACK", JSON.stringify(formInput))
+
+}
+
+function onSubmitClick(evt) {
+    const saveMessage = JSON.parse(localStorage.getItem("FEEDBACK"));
+    console.log(saveMessage.email);
+    console.log(saveMessage.message);
+        evt.target.reset();
+        console.log(evt);
+        localStorage.removeItem("FEEDBACK");
+    }
+
+function onFormClick(evt) {
+    const saveMessage = JSON.parse(localStorage.getItem("FEEDBACK"));
+    console.log(saveMessage);
+    if (saveMessage) {
+        refs.email.value = saveMessage.email,
+            refs.textarea.value = saveMessage.message
+    
+    }
+}
