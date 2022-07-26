1223591ff402667add7c7ad78ebe9c94ab49b7d3
Nataliya Novak <novak.natali@gmail.com>
2022-07-26 21:56:00 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,7 +5,9 @@ const iframe = document.querySelector('iframe');
 
 player.on('timeupdate', throttle(getTime, 100));
 function getTime(currentTime) {
-     localStorage.setItem("videoplayer-current-time", currentTime.seconds)   
+    if ("videoplayer-current-time") {
+        localStorage.setItem("videoplayer-current-time", currentTime.seconds)
+    }
 };
 player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
     player.getVideoTitle().then(function(title) {
